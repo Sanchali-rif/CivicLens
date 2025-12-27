58bf17bb88ac1e6ec4b6525aa83eb861522b07aa
DebadritaGhosal <khushighosal06@gmail.com>
2025-12-28 00:13:57 +0530

    .

--- backend/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('UPLOAD_DIR', __DIR__ . '/uploads/');
-define('PYTHON_BIN', '/usr/bin/python3');   
-define('VISION_SCRIPT', __DIR__ . '/vision_test.py');
-define('GOOGLE_CREDENTIALS', __DIR__ . '/service-account.json');
-?>
