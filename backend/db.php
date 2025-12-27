9b221ff3f40d5db9376fbccf70f498452ec2ee51
DebadritaGhosal <khushighosal06@gmail.com>
2025-12-28 00:02:11 +0530

    backend

---
+++ backend/db.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config.php';
+try
+{
+    $pdo= new PDO("mysql:host=localhost;dbname=civiclens;charset=utf8","root","");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e)
+{
+    die("DB connection failed");
+}
